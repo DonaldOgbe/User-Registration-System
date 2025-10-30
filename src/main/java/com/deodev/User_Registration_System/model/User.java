ae7a14d23861407b7fd05c53a80ebf2f92c48596
Donald Ogbe <ogbeebimobowei@gmail.com>
2025-10-30 17:22:59 +0100

    added lombok, and deleted old code

--- src/main/java/com/deodev/User_Registration_System/model/User.java
+++ src/main/java/com/deodev/User_Registration_System/model/User.java
@@ -1,46 +1,13 @@
 package com.deodev.User_Registration_System.model;
 
-import java.util.Objects;
+import lombok.*;
 
-
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String name;
     private String email;
     private long id;
-    private static long idCount = 1;
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email = email;
-        id = hashCode();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, email);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-
-        User user = (User) obj;
-        return name.equals(user.name) && email.equals(user.email) && id == ((User) obj).getId();
-    }
-
-
 }
--- src/test/java/com/deodev/User_Registration_System/service/EmailServiceTest.java
+++ src/test/java/com/deodev/User_Registration_System/service/EmailServiceTest.java
@@ -12,28 +12,4 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailServiceTest {
-    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private final PrintStream originalStream = System.out;
-
-    EmailService testEmailService;
-
-    @BeforeEach
-    void setup(){
-        System.setOut(new PrintStream(outputStream));
-        testEmailService = new EmailService();
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setOut(originalStream);
-    }
-
-    @Test @Description("print welcome message to console")
-    void getUserNameAndPrintToConsole() {
-        User testUser = new User("User", "user@gmail.com");
-        testEmailService.sendWelcomeEmail(testUser);
-        String message = String.format("Welcome %s !! We are Happy to have you.%n", testUser.getName());
-        assertEquals(outputStream.toString().trim(), (message.trim()));
-    }
-
 }
