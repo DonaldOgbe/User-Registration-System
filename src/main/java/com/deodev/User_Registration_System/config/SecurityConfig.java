5a3e7b97700359d4728ab4ba473773f0ee1e779c
Donald Ogbe <ogbeebimobowei@gmail.com>
2025-11-06 19:38:37 +0100

    feature(swagger doc) add swagger doc and dev config - added swagger docs and a dev config file and updated the datasource in dev - Implements swagger api testing and using multiple app config environments

--- src/main/java/com/deodev/User_Registration_System/config/SecurityConfig.java
+++ src/main/java/com/deodev/User_Registration_System/config/SecurityConfig.java
@@ -21,11 +21,8 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .authenticationProvider(authenticationProvider)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
